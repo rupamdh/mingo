ae55e91c56a983a92b8a2cf10f4640a7d5d608dd
Rupam Dhara <rupamdhara.1579@gmail.com>
2024-07-18 23:20:53 +0530

    Updates

--- library/admin.py
+++ library/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Book)
+# admin.site.register(Book)
 admin.site.register(Author)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_date', 'price', 'status')
+
+
+admin.site.register(Book, BookAdmin)
