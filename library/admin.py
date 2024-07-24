b1b4e12c70259f546ad7ace48f53904b9f7a96c0
rupam office <rupamdhara.1579@gmail.com>
2024-07-24 18:57:53 +0530

    Updates

--- library/admin.py
+++ library/admin.py
@@ -9,6 +9,8 @@ admin.site.register(Author)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_date', 'price', 'status')
     search_fields = ('title', 'price')
+    list_filter = ('status', 'author')
+    list_per_page = 10
 
 
 admin.site.register(Book, BookAdmin)
--- mingo/admin.py
+++ mingo/admin.py
@@ -3,18 +3,11 @@ from django import forms
 from django.contrib.admin import AdminSite
 from .forms import CustomAuthenticationForm, BaseAdminForm, MingoPasswordChangeForm
 from django.contrib.admin import ModelAdmin
+from django.urls import NoReverseMatch, Resolver404, resolve, reverse
+
 
 #Change the default login form
 AdminSite.login_form = CustomAuthenticationForm
-
-# def change_password_change(original_method, user_id):
-#     def wrapper(self, request, user_id, form_class=MingoPasswordChangeForm, extra_context=None):
-#         extra_context = extra_context or {}
-#         extra_context['title'] = 'Change Password'
-#         return original_method(self, request, user_id, form_class, extra_context)
-#     return wrapper
-
-# AdminSite.password_change = change_password_change(AdminSite.password_change)
 
 #Change the default change form for model
 ModelAdmin.form = BaseAdminForm
@@ -27,4 +20,25 @@ def change_formfield_for_foreignkey(original_method):
         return original_method(self, db_field, request, **kwargs)
     return wrapper
 ModelAdmin.formfield_for_foreignkey = change_formfield_for_foreignkey(ModelAdmin.formfield_for_foreignkey)
+
+
+#Change the default change password form for user
+# def replace_password_change_form(original_method):
+#     def wrapper(self, request, extra_context=None):
+#         from django.contrib.auth.views import PasswordChangeView
+#         defaults = {
+#             "form_class": MingoPasswordChangeForm,  # Use your custom form class
+#             "extra_context": {**self.each_context(request), **(extra_context or {})},  # Additional context for the template
+#         }
+#         if self.password_change_template is not None:
+#             defaults["template_name"] = self.password_change_template
+#         request.current_app = self.name
+#         return PasswordChangeView.as_view(**defaults)(request)
+#     return wrapper
+
+# # Apply the monkey patch to AdminSite.password_change
+# AdminSite.password_change = replace_password_change_form(AdminSite.password_change)
+
+
+
 
